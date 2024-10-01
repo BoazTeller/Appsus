9d2431509b05e376ec526060e49394cc53269c85
BillOfBalrog <boaz.teller@gmail.com>
2024-10-02 01:13:46 +0300

    Add nested routes, NotFound, and UserMsg components to App

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,24 +1,45 @@
 const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
+// Main App Components
 import { AppHeader } from "./cmps/AppHeader.jsx"
-import { About } from "./pages/About.jsx"
+
+// Page Views
 import { Home } from "./pages/Home.jsx"
+import { About } from "./pages/About.jsx"
+
+// Mail App Components
 import { MailIndex } from "./apps/mail/pages/MailIndex.jsx"
+import { MailDetails } from "./apps/mail/pages/MailDetails.jsx"
+
+// Note App Components
 import { NoteIndex } from "./apps/note/pages/NoteIndex.jsx"
 
-
+// Utility Components (UserMsg, NotFound)
+import { UserMsg } from "./cmps/UserMsg.jsx"
+import { NotFound } from "./cmps/NotFound.jsx"
 
 export function App() {
-    return <Router>
-        <section className="app">
-            <AppHeader />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/mail" element={<MailIndex />} />
-                <Route path="/note" element={<NoteIndex />} />
-            </Routes>
-        </section>
-    </Router>
+    return (
+        <Router>
+            <main className="app">
+                <AppHeader />
+                
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+
+                    <Route path="/mail" element={<MailIndex />} >
+                        <Route path="/mail/:mailId" element={<MailDetails />} />
+                    </Route>
+
+                    <Route path="/note" element={<NoteIndex />} />
+                
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
+
+            <UserMsg />
+        </Router>
+    )
 }
