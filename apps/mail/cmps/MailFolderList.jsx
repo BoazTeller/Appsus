212580c5d893a6c2a64821709d238f2665b61135
BillOfBalrog <boaz.teller@gmail.com>
2024-10-02 01:58:27 +0300

    Implement MailFolderList (filter) sidebar

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -1,6 +1,18 @@
 {/* <MailFolderList>
 • Allow filtering by different folders: inbox / sent / trash/ draft */}
 
-export function MailList() {
-    return <div>Mail list</div>
+export function MailFolderList() {
+
+    return (
+        <div class="sidebar">
+            <button class="compose-btn">Compose</button>
+            <ul class="menu">
+                <li class="menu-item active"><span>Inbox</span></li>
+                <li class="menu-item"><span>Starred</span></li>
+                <li class="menu-item"><span>Sent</span></li>
+                <li class="menu-item"><span>Drafts</span></li>
+                <li class="menu-item"><span>Trash</span></li>
+            </ul>
+        </div>
+    )
 }
