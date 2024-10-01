fe17851e91c16ecfed1e24688a634bcabefc77ba
BillOfBalrog <boaz.teller@gmail.com>
2024-10-02 01:18:29 +0300

    Add placeholder components for Note feature structure

---
+++ apps/note/cmps/NotePreview.jsx
@@ -0,0 +1,21 @@
+// This component renders the <NotePreview> component that allow viewing the notes
+// preview, and also changing color, pin, etc.
+
+// The <NotePreview> component, uses a dynamic component to show different types
+// of notes:
+//      o <NoteTxt>
+//      o <NoteImg>
+//      o <NoteVideo>
+//      o <NoteTodos>
+
+// • Extra:
+//      o <NoteAudio>
+//      o <NoteCanvas>
+//      o <NoteMap>
+
+// • Bonus:
+//      o <NoteRecording>
+
+export function NotePreview() {
+    return <div>note list</div>
+}
