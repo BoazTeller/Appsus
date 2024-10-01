0036517f344ad70aeb514f98a42234647a2c015c
BillOfBalrog <boaz.teller@gmail.com>
2024-10-02 01:59:29 +0300

    Initialize MailIndex with folder list and load mail logic

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -3,7 +3,39 @@
 • Renders the list and the filter components (both top filter with search,
 and side filter for different folders) */}
 
+import { mailService } from "../services/mail.service.js"
+
+import { MailFolderList } from "../cmps/MailFolderList.jsx"
+
+const { useState, useEffect } = React
+
 export function MailIndex() {
-    return <div>mail app</div>
+
+    const [mails, setMails] = useState(null)
+    const [filterBy, setFilterBy] = useState(mailService.getDefaultFilter())
+
+    useEffect(() => {
+        loadMails()
+    }, [])
+
+    useEffect(() => {
+        console.log(mails)
+    }, [mails])
+
+    function loadMails() {
+        mailService.query()
+            .then(mails => {
+                setMails(mails)
+            })
+    }
+
+    return (
+        <section className="mail-index">
+            <MailFolderList />
+
+        </section>
+    )
 }
+
+
 
