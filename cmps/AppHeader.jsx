113c17b5b3cc5c327decb721a857a5e8edf55004
BillOfBalrog <boaz.teller@gmail.com>
2024-10-02 01:09:04 +0300

    Refactor AppHeader with dynamic nav links

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -2,15 +2,31 @@ const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
 
-    return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO!</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
-    </header>
+    const navLinks = [
+        { title: 'Home', path: '' },
+        { title: 'About', path: 'about' },
+        { title: 'Mail', path: 'mail' },
+        { title: 'Note', path: 'note' }
+    ]
+
+    return (
+        <header className="app-header">
+            <Link to="/">
+                <h3 className="logo">Appsus</h3>
+            </Link>
+
+            <nav className="app-nav">
+                {navLinks.map(navLink => 
+                    <NavLink
+                        key={navLink.path}
+                        to={`/${navLink.path}`}
+                        className="nav-link"
+                        title={`Go to ${navLink.title} page`}
+                    >
+                        {navLink.title}
+                    </NavLink>
+                )}
+            </nav>
+        </header>
+    )
 }
