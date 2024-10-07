e5a6cb5fa454da05cfd62415705e36ac61db67af
tal.peretz <tal.peretz@tufin.com>
2024-10-07 22:06:33 +0300

    fixing some css

--- apps/note/cmps/NoteImg.jsx
+++ apps/note/cmps/NoteImg.jsx
@@ -5,11 +5,12 @@ export function NoteImg({ onDeleteNoteClick, onEditNoteClick, note }) {
     return (
         <div className="card image-card" onClick={() => onEditNoteClick(note)}>
             <Dropdown onDeleteNoteClick={onDeleteNoteClick} onEditNoteClick={onEditNoteClick} note={note}></Dropdown>
-            <h2>{note.info.title}</h2>
             <img className="note-img" src={note.info.url} />
+            {note.info.title && (
+                <h2 className="note-title">{note.info.title}</h2>
+            )}
             <div className="card-navbar">
                 <button className="fa fa-trash delete-btn" onClick={(ev) => onDeleteNoteClick(ev, note.id)}></button>
-
             </div>
         </div>
     )
