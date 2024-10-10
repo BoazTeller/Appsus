0fddbdef7524daea898258a091b2db4daf409323
BillOfBalrog <boaz.teller@gmail.com>
2024-10-11 01:51:56 +0300

    Add styling to home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,19 @@
-
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
-    </section>
+    
+    return ( 
+        <section className="home">
+            <header className="logo">
+                <span>A</span>
+                <span>p</span>
+                <span>p</span>
+                <span>s</span>
+                <span>u</span>
+                <span>s</span>
+            </header>
+
+            <h1>Welcome to our home page!</h1>
+
+            <footer>Powered by Appsus — where emails and notes find their home!</footer>
+        </section>
+    )
 }
