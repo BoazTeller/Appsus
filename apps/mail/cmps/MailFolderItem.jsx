2e3b98e80fad62984de107d3a9bff06d45640f37
BillOfBalrog <boaz.teller@gmail.com>
2024-10-12 08:27:01 +0300

    Add FolderItem cmp for MailFolderList refactor

---
+++ apps/mail/cmps/MailFolderItem.jsx
@@ -0,0 +1,15 @@
+export function MailFolderItem(props) {
+    const { folderItem, handleFilter, currFolder } = props
+
+    return (
+        <button 
+            key={folderItem.key}
+            className={`btn-${folderItem.key} btn-folder ${currFolder === folderItem.key ? 'active' : ''}`}
+            onClick={() => handleFilter(folderItem.key)}
+        >
+            <div className={`materials`}>{folderItem.icon}</div>
+            <span className="label">{folderItem.label}</span>
+            {folderItem.count !== undefined && <span className="unread">{folderItem.count || ''}</span>}
+        </button>
+    )
+}
