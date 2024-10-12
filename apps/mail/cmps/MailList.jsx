7d20ad8fd5061b40a6c9d0cc2a52b834db3e547f
BillOfBalrog <boaz.teller@gmail.com>
2024-10-12 08:32:23 +0300

    Add empty folder messages for imbox/sent/starred/trash/drafts while trying to stay close to the original ones

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,15 +1,44 @@
+const { useEffect, useRef, Fragment } = React
 const { Link } = ReactRouterDOM
 
 import { MailSort } from "./MailSort.jsx"
 import { MailPreview } from "./MailPreview.jsx"
+import { Loader } from "./Loader.jsx"
 
 export function MailList({
-    mails, filterBy, sortBy, folder, isLoading,
+    mails, filterBy, sortBy, folder, isLoading, onOpenMailEdit,
     onRemoveMail, onToggleStarred, onSetFilterBy, onSetSortBy
 }) {
 
+    const mailListRef = useRef(null)
+
+    // useEffect(() => {
+    //     if (mailListRef.current) {
+    //         mailListRef.current.style.height = `calc(100vh - 181px)`;
+    //     }
+    // }, [mails]) 
+
+    function getEmptyFolderMessage() {
+        switch (folder) {
+            case 'inbox':
+                return 'Your inbox is empty.'
+            case 'sent':
+                return <Fragment>No sent messages! <a href="#" className="send-link" onClick={onOpenMailEdit}>Send</a> one now!</Fragment>
+            case 'starred':
+                return 'No starred messages. Stars let you give messages a special status to make them easier to find. ' + 
+                       'To star a message, click on the star outline beside any message or conversation.'
+            case 'trash':
+                return 'No conversations in Trash folder.'
+            case 'drafts':
+                return 'You don\'t have any saved drafts.\n' +
+                       'Saving a draft allows you to keep a message you aren\'t ready to send yet.'
+            default:
+                return `No mails in the ${folder} folder.`
+        }
+    }
+    
     return (
-        <React.Fragment>
+        <Fragment>
             <MailSort 
                 onSetSortBy={onSetSortBy}
                 sortBy={sortBy} 
@@ -17,14 +46,8 @@ export function MailList({
                 filterBy={filterBy} 
             />
 
-            <ul className="mail-list clean-list">
-                {isLoading && (
-                    <span className="loader1"></span>
-                )}
-
-                {!isLoading && (!mails || mails.length === 0) && (
-                    <p className="empty-folder-msg">No mails in {folder} folder.</p>
-                )}
+            <ul className="mail-list clean-list" ref={mailListRef}>
+                {isLoading && <Loader />}
 
                 {!isLoading && mails && mails.length > 0 && (
                     mails.map(mail => (
@@ -39,7 +62,11 @@ export function MailList({
                         </li>
                     ))
                 )}
+
+                {!isLoading && (!mails || mails.length === 0) && (
+                    <p className="empty-folder-msg">{getEmptyFolderMessage()}</p>
+                )}
             </ul>
-        </React.Fragment>
+        </Fragment>
     )
 }
